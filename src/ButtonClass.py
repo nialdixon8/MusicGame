084fc434a8f3bb752f9186bf1a2d3086f69d995d
Nial Dixon <nialdixon8@gmail.com>
2021-12-11 23:41:32 +0000

    Added button class to src

---
+++ src/ButtonClass.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Button:
+    def __init__(self, size, pos, colour):
+        self.pos  = pos
+        self.size = size
+        self.colour = colour
+
+    def render(self, window):
+        #window.blit(self.button, (self.pos[0], self.pos[1]))
+        pygame.draw.circle(window, self.colour, (self.pos[0], self.pos[1]), self.size)
+
+    def clicked(self, events):
+        mousePos = pygame.mouse.get_pos()#  get the mouse position
+        for event in events:
+            if self.button.get_rect(topleft=self.pos).collidepoint(mousePos[0], mousePos[1]):
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    return True
+        return False
